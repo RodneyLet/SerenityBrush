434217e0a7a9931fa56f195604429b418df0beca
Rodney Lettman <your.email@example.com>
2026-07-12 21:21:07 -0500

    Remove duplicate SafetyProvider.js

--- src/components/SafetyProvider.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = SafetyProvider;
-function SafetyProvider(_a) {
-    var children = _a.children;
-    return <>{children}</>;
-}
